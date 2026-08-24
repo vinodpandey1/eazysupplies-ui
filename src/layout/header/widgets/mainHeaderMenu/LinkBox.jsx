5e60981cef964c3dc063c3adb92f8734700f36d6
vinodpandey <vinod@dmayant.com>
2026-08-24 14:22:52 +0530

    fix absolute collection menu links

--- src/layout/header/widgets/mainHeaderMenu/LinkBox.jsx
+++ src/layout/header/widgets/mainHeaderMenu/LinkBox.jsx
@@ -54,7 +54,7 @@ const LinkBox = ({ menu, onNavigate }) => {
       ) : (
         <>
           {menu.link_type == "link" && menu.is_target_blank === 0 ? (
-            <Link onClick={onNavigate} className="dropdown-item" href={menu?.path.charAt(0) == "/" ? `collections?layout=collection_3_grid&${menu?.path.includes("category") ? "category=" + menu?.id + "&title=" +menu?.title : "brand=" + menu.id + "&title=" +menu?.title }` : `/${menu?.path}`}>
+            <Link onClick={onNavigate} className="dropdown-item" href={menu?.path.charAt(0) == "/" ? `/collections?layout=collection_3_grid&${menu?.path.includes("category") ? "category=" + menu?.id + "&title=" + menu?.title : "brand=" + menu.id + "&title=" + menu?.title}` : `/${menu?.path}`}>
               {menu.title}
               {menu.badge_text && <label className={`menu-label ${menu?.badge_color ? menu?.badge_color : ''}`}>{menu?.badge_text}</label>}
             </Link>
